59d0c57d6895b8782a1dbf9045d53c1098eaa190
AKIYA <157039733+akiyahiro@users.noreply.github.com>
2025-03-27 18:34:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'AKIYA',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "AKIYA OFFICIAL",
+  botOwner: 'AKILA SANDARUWAN', // Your name
+  ownerNumber: '94767973534', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
